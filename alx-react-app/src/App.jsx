e2befd5ed2fb31d14f884fda3a01a86c9bfcbe52
Lerato Masenya <masenyalerato270@gmail.com>
2025-02-21 08:12:33 +0200

    Fixed import and JSX placement issues

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from '/Users/my pc/alx-fe-reactjs/alx-react-app/src/WelcomeMessage'
+import WelcomeMessage from '/Users/my pc/alx-fe-reactjs/alx-react-app/src/components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
