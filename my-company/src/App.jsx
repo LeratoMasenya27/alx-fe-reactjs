4e0102c91ea9b6cc70f8cd3e438b742d436d5f72
Lerato Masenya <masenyalerato270@gmail.com>
2025-03-02 19:56:53 +0200

    final task

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0)
