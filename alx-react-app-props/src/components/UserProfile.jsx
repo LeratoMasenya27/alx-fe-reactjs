348b765ad96243bb62d6fee240f247e8efca8fe9
Lerato Masenya <masenyalerato270@gmail.com>
2025-03-02 19:23:40 +0200

    user profile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
-const UserProfile = (props) => {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '10px', borderRadius: '8px', boxShadow: '2px 2px 10px rgba(0,0,0,0.1)', maxWidth: '300px' }}>
-      <h2 style={{ color: 'blue', marginBottom: '5px' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{userData.name}</h2>
+      <p>Age: <span style={{ fontWeight: "bold" }}>{userData.age}</span></p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
