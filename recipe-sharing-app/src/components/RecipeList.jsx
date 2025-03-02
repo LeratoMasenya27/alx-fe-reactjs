51f0dd5836e5d9bfe6091e99cffc7fc5109a1c7b
Lerato Masenya <masenyalerato270@gmail.com>
2025-03-02 20:16:55 +0200

    new project

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,18 @@
+import useRecipeStore from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
